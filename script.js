f75437c7d83099f1f6e64a8486a2f7905f98fb65
a2uuz <aziznageeye9@gmail.com>
2021-03-25 18:25:51 +0300

    googleapimap

---
+++ script.js
@@ -0,0 +1,11 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 9.57153, lng: 	44.05330 },
+    zoom: 8,
+  });
+  var marker = new google.maps.marker({
+      
+  })
+}
